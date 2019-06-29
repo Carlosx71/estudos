4103e096650dc42fb109bf78b2e38122fa00674b
Carlos Santos <carlos.x71@hotmail.com>
2019-06-29 16:37:55 -0300

    Finalizado

--- EstudosPython/meuPrograma2.py
+++ EstudosPython/meuPrograma2.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 color = { 
     'clear':'\033[m',
@@ -28,30 +29,78 @@ cont = 1
 
 listPart = []
 
+print(f'{colorBold["yellow"]}|{"=-"*40}|{color["clear"]}')
 while cont <= 8:
 
     listPart.append(str(input(f'{colorBold["blue"]}{cont}º participante: {color["clear"]}')))
     cont = cont + 1
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')    
 
-print("\n")
 random.shuffle(listPart)
 
 contVs = 0
 contVs2 = 0
-contT = 0
 
-while contVs < 4:
-    if contVs % 2:
-        print(f'{color["yellow"]}Jogador {listPart[contVs]} {contVs} vs Jogador {listPart[contVs2]} {contVs2}{color["clear"] }a')
-        contVs = contVs + 1
-        contVs2 = contVs2 + 2
-    else:
-        print(f'{color["green"]}Jogador {listPart[contVs2]} {contVs} vs Jogador {listPart[contVs]} {contVs2}{color["clear"]}v')
-        contVs = contVs + 1
-        contVs2 = contVs2 + 2
-    contT = contT + 1
-    print(contT)
+print('Jogadores Selecionados')
+while contVs < 8:
+    contVs = contVs + 1    
+    while contVs > contVs2:
+        print(f'{color["yellow"]}Jogador {colorBold["grey"]}{listPart[contVs]}{color["clear"]} {color["lightBlue"]}vs{color["clear"]} {color["yellow"]}Jogador {colorBold["grey"]}{listPart[contVs2]}{color["clear"]}') 
+        contVs2 = contVs2 + 2    
 
-print(listPart)
+escolhido1 = random.choice(listPart)
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')  
+input(f'{colorBold["pink"]}Vai la! Joguem a proxima rodada...{color["clear"]}')
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')
+input(f'Estou torcendo por voce {escolhido1}')  
+print('Quem foram os vencedores?')
 
-#198.153.194.1
+del(listPart[0:])
+
+contSemi = 0
+while contSemi < 4:
+    contSemi = contSemi + 1
+    listPart.append(str(input(f'{colorBold["grey"]}{contSemi}º Semifinalista: {color["clear"]}')))
+
+
+random.shuffle(listPart)
+
+contVs3 = 0
+contVs4 = 0
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')
+print('Proxima Rodada')
+while contVs3 < 4:
+    contVs3 = contVs3 + 1
+    while contVs3 > contVs4:
+        print(f'{color["yellow"]}Jogador {colorBold["grey"]}{listPart[contVs3]}{color["clear"]} {color["lightBlue"]}vs{color["clear"]} {color["yellow"]}Jogador {colorBold["grey"]}{listPart[contVs4]}{color["clear"]}')
+        contVs4 = contVs4 + 2
+
+listFinal = []
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')
+print('Quem foram os vencedores das semifinais?')
+
+listFinal.append(str(input(f'{colorBold["blue"]}1º participante: {color["clear"]}')))
+listFinal.append(str(input(f'{colorBold["blue"]}2º participante: {color["clear"]}')))
+print(f'Boa sorte {listFinal[0]} e {listFinal[1]}!')
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')
+
+escolhido = random.choice(listFinal)
+input(f'{colorBold["green"]} Eu pessoalmente estou torcendo por voce {escolhido}')
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')
+vencedor = input(f'{colorBold["blue"]} E ai? Quem ganhou? {color["clear"]}')
+
+print(f' Parabens {color["lightBlue"]}{vencedor}!{color["clear"]} voce e incrivel!!!')
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')
+vencNum = input(' Se quer ganhar um premio, digite 1, se nao digite 0: ')
+
+if vencNum.isnumeric():
+    vencNum = int(vencNum)
+    if vencNum == 1:
+        print(' Parabens! Pedi o "organizador" para buscar seu CHOCOLATE')
+    elif vencNum == 0:
+        print(' Que pena, pedeu o chocolate =/')
+else:
+    print(' Nao tente me enganar! Por isso, nao vai ganhar um chocolate')
+
+print('Voces sao incriveis! Obrigado por jogar!!!')
+print(f'|{colorBold["yellow"]}{"=-"*40}|{color["clear"]}')
