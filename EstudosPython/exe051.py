5f66e422e66fd3f1d6dec843c950bd41b42086ae
Carlos Santos <carlos.x71@hotmail.com>
2019-08-28 22:00:42 -0300

    teste commit

--- EstudosPython/exe051.py
+++ EstudosPython/exe051.py
@@ -2,6 +2,7 @@ from win10toast import ToastNotifier
 
 termo = int(input('Digite o termo: '))
 razao = int(input('Digite a razao: '))
+
 decimo = termo + (10-1) * razao
 for x in range(termo, decimo + razao, razao ):
     print(x)
