4953df932059486b56e749d12334e12bd84232b2
Carlos Santos <carlos.x71@hotmail.com>
2019-08-29 00:37:11 -0300

    ico

--- EstudosPython/exe051.py
+++ EstudosPython/exe051.py
@@ -1,4 +1,5 @@
 from win10toast import ToastNotifier 
+import PIL.Image
 
 termo = int(input('Digite o termo: '))
 razao = int(input('Digite a razao: '))
@@ -7,5 +8,4 @@ decimo = termo + (10-1) * razao
 for x in range(termo, decimo + razao, razao ):
     print(x)
 
-toaster = ToastNotifier()
-toaster.show_toast("Sample Notification","Python is awesome!!!", duration=2)
+ToastNotifier().show_toast("Sample Notification","Python is awesome!!!", icon_path = 'emoji_emoticon.ico', duration=60)
